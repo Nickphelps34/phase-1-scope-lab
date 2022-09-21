63148abbfe76cf2c67f2e7ff679b656ead4dd796
Nick Phelps <nickphelps34@gmail.com>
2022-09-21 10:15:36 -0400

    commit tree 57a4139504df409f0a0e4c8f6f97a87c92dc8b7f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+const leastFavoriteCustomer = "Nick"
+
+function upperCaseCustomerName(){
+    return customerName = "BOB"
+};
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "not Nick"
+}
